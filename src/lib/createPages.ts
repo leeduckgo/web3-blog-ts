f09942334d8cfe6803ea435f136f1ba44f2201c1
leeduckgo <albertschr@163.com>
2021-10-06 10:32:49 +0800

    feat/init

---
+++ src/lib/createPages.ts
@@ -0,0 +1,62 @@
+import path from "path"
+import { GatsbyCreatePages } from "../types"
+
+interface Post {
+  node: {
+    fields: {
+      slug: string
+    }
+  }
+}
+
+export const createPages: GatsbyCreatePages = async ({
+  graphql,
+  boundActionCreators,
+}) => {
+  const { createPage } = boundActionCreators
+
+  const allMarkdown = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { fields: [frontmatter___date], order: DESC }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (allMarkdown.errors) {
+    throw allMarkdown.errors
+  }
+
+  // Create blog posts pages.
+  const posts = allMarkdown.data.allMarkdownRemark.edges
+
+  posts.forEach((post: Post, index: number) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node
+    const next = index === 0 ? null : posts[index - 1].node
+
+    createPage({
+      path: post.node.fields.slug,
+      // tslint:disable-next-line:object-literal-sort-keys
+      component: path.resolve(`./src/templates/blog-post.tsx`),
+      context: {
+        next,
+        previous,
+        slug: post.node.fields.slug,
+      },
+    })
+  })
+
+  return null
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,84 @@
+import { graphql, PageRendererProps, useStaticQuery } from "gatsby"
+import React from "react"
+import styled from "styled-components"
+import { Bio } from "../components/bio"
+import { Layout } from "../components/layout"
+import { FadeLink } from "../components/link"
+import { SEO } from "../components/seo"
+import { MarkdownRemark } from "../graphql-types"
+import { rhythm } from "../utils/typography"
+
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+const StyledLink = styled(FadeLink)`
+  box-shadow: none;
+`
+
+const Title = styled.h3`
+  margin-bottom: ${rhythm(1 / 4)};
+`
+
+type Props = PageRendererProps
+
+const BlogIndex = (props: Props) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+      allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+        edges {
+          node {
+            excerpt
+            fields {
+              slug
+            }
+            frontmatter {
+              date(formatString: "MMMM DD, YYYY")
+              title
+              description
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMarkdownRemark.edges
+
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO
+        title="All posts"
+        keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+      />
+      <Bio />
+      {posts.map(({ node }: { node: MarkdownRemark }) => {
+        const frontmatter = node!.frontmatter!
+        const fields = node!.fields!
+        const slug = fields.slug!
+        const excerpt = node!.excerpt!
+
+        const title = frontmatter.title || fields.slug
+        return (
+          <div key={slug}>
+            <Title>
+              <StyledLink to={slug}>{title}</StyledLink>
+            </Title>
+            <small>{frontmatter.date}</small>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: frontmatter.description || excerpt,
+              }}
+            />
+          </div>
+        )
+      })}
+    </Layout>
+  )
+}
+
+export default BlogIndex
