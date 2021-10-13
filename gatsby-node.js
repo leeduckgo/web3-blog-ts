ad0701decf9ea0f24e9c4f677aeb3af2594e1ded
zombieliu <17600047539@163.com>
2021-10-13 15:54:38 +0800

    fix small error

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,3 +17,18 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 }
 
 exports.createPages = require("./src/lib/createPages").createPages
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /@toruslabs/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
